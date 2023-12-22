23d7ca87eea7a594e224e4a1f068850b7c035f71
vanntrong <vantrongbrv@gmail.com>
2023-12-22 21:40:01 +0700

    chore: change declare module

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'react-native-progress' {
+declare module '@vanntrong/react-native-progress' {
   import React from 'react';
   import { TextStyle, ViewProperties } from 'react-native';
 
